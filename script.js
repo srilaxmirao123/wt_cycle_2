dcfc4710caacb83a626b98835cb7d3b2502a4059
srilaxmirao123 <srilaxmijupally45@gmail.com>
2026-02-17 14:56:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+    function detectDevice() {
+  const deviceType = document.getElementById("deviceType");
+  const featureList = document.getElementById("featureList");
+
+  if (window.innerWidth < 768) {
+    deviceType.textContent = "You are using a mobile device.";
+    featureList.innerHTML = `
+      <div class="bg-white p-4 border rounded">Mobile Feature 1</div>
+      <div class="bg-white p-4 border rounded">Mobile Feature 2</div>
+    `;
+  } 
+  else if (window.innerWidth < 1024) {
+    deviceType.textContent = "You are using a tablet.";
+    featureList.innerHTML = `
+      <div class="bg-white p-4 border rounded">Tablet Feature 1</div>
+      <div class="bg-white p-4 border rounded">Tablet Feature 2</div>
+      <div class="bg-white p-4 border rounded">Tablet Feature 3</div>
+    `;
+  } 
+  else {
+    deviceType.textContent = "You are using a desktop.";
+    featureList.innerHTML = `
+      <div class="bg-white p-4 border rounded">Desktop Feature 1</div>
+      <div class="bg-white p-4 border rounded">Desktop Feature 2</div>
+      <div class="bg-white p-4 border rounded">Desktop Feature 3</div>
+      <div class="bg-white p-4 border rounded">Desktop Feature 4</div>
+    `;
+  }
+}
+
+window.onload = detectDevice;
+window.onresize = detectDevice;
